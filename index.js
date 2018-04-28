bae7225fd1de4295b813092d1cf4ba1e5d185704
Rowan Lucey <rslucey@gmail.com>
2018-04-28 19:59:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,9 @@ function logShout(string) {
 function logWhisper(string) {
   console.log('Hello'.toLowerCase())
 }
+
+function sayHiToGrandma(string) {
+  if(string === lowercase) {
+    return "I can't hear you!"
+  }
+}
